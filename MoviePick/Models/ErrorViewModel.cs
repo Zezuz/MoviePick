64d2e5c40a97ef256c85af5bc389985a08679307
Zezuz <mczanaty92@gmail.com>
2021-06-29 20:09:04 -0500

    Add project files.

---
+++ MoviePick/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MoviePick.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
